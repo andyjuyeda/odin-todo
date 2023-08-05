b2d63b3174656fbc3f6e5eef5fa2c34f23449514
Andy Uyeda <andyjuyeda2@gmail.com>
2023-08-04 21:44:10 -0700

    style header and create modal for new tasks

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import './style.css';
-import * as content from './domelements';
+import "./style.css";
+import MicroModal from 'micromodal';
+import * as content from "./domelements";
 
-const container = document.querySelector('#content');
+MicroModal.init();
+
+const container = document.querySelector("#content");
 const updateContent = (contentFunc) => {
-    container.innerHTML = "";
-    container.appendChild(contentFunc());
-}
+  container.innerHTML = "";
+  container.appendChild(contentFunc());
+};
 
-let overviewButton = document.querySelector('.nav-btn.overview');
-overviewButton.addEventListener('click', () => updateContent(content.overviewContent));
+let overviewButton = document.querySelector(".nav-btn.overview");
+overviewButton.addEventListener("click", () =>
+  updateContent(content.overviewContent)
+);
 
-let todoButton = document.querySelector('.nav-btn.todo');
-todoButton.addEventListener('click', () => updateContent(content.todoContent));
+let todoButton = document.querySelector(".nav-btn.todo");
+todoButton.addEventListener("click", () => updateContent(content.todoContent));
 
-let inProgressButton = document.querySelector('.nav-btn.in-progress');
-inProgressButton.addEventListener('click', () => updateContent(content.inProgressContent));
+let inProgressButton = document.querySelector(".nav-btn.in-progress");
+inProgressButton.addEventListener("click", () =>
+  updateContent(content.inProgressContent)
+);
 
-updateContent(content.overviewContent());
+updateContent(content.overviewContent);
